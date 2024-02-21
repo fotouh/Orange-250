968c40207b5eee97b94f81b2829ff7cf00233840
fotouh <fotouhmohamed2002@gmail.com>
2024-02-21 09:23:12 +0200

    last Update

---
+++ javascript/main.js
@@ -0,0 +1,25 @@
+let x = document.querySelectorAll("td");
+for (let i = 0; i < x.length; i++) {
+  if (x[i].innerText === "Yes") {
+    x[i].style.backgroundColor = "#198754";
+  }
+}
+
+function myFunction() {
+  const input = document.getElementById("myInput");
+  const filter = input.value.toUpperCase();
+  const table = document.getElementById("myTable");
+  const rows = table.getElementsByTagName("tr");
+
+  for (let i = 0; i < rows.length; i++) {
+    const td = rows[i].getElementsByTagName("td")[0]; // Assuming you want to search by the first column (Name)
+    if (td) {
+      const txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        rows[i].style.display = ""; // Show matching rows
+      } else {
+        rows[i].style.display = "none"; // Hide non-matching rows
+      }
+    }
+  }
+}
